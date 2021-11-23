ffd5c64709b7cfadce14afd6067e1101f9d2ef81
Thoreex <14024927+Thoreex@users.noreply.github.com>
2021-11-22 22:59:10 -0600

    Se agrega migracion de scripts de base de datos

--- scripts/V1__create_tbl_user.sql
+++ scripts/V1__create_tbl_user.sql
@@ -1,7 +1,3 @@
-BEGIN TRANSACTION;
-
-DROP TABLE IF EXISTS tbl_user;
-
 CREATE TABLE tbl_user (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     username VARCHAR(32) UNIQUE NOT NULL,
@@ -12,5 +8,3 @@ CREATE TABLE tbl_user (
 
 INSERT INTO tbl_user (username, password, first_name, last_name)
 VALUES ('admin', 'pbkdf2:sha256:260000$YAw3lhu3TjFjxfsz$59bac94ff7b09a12133a6dec8f23defe8a03d30e87c394cb89a74056ef31381a', 'Administrator', 'User');
-
-END TRANSACTION;
